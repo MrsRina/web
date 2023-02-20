1ebcb8da94e46a55260986466424965e5c58eb75
Rina <37088203+MrsRina@users.noreply.github.com>
2023-02-20 16:25:24 -0300

    Maybe an issue with my brain.

--- visual/webglcontext.js
+++ visual/webglcontext.js
@@ -1,5 +1,8 @@
 var canvas = document.getElementById("webgl-context")
 var gl = canvas.getContext("webgl")
 
+canvas.width = document.body.clientWidth;
+canvas.height = document.body.clientHeight;
+
 gl.clearColor(0.0, 0.0, 1.0, 1.0);
 gl.clear(gl.COLOR_BUFFER_BIT)
