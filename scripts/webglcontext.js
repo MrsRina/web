445f122f0dc230fb63a74d95b2bf0201b68c86e1
Rina <37088203+MrsRina@users.noreply.github.com>
2023-04-26 04:15:14 -0300

    ~...

--- scripts/webglcontext.js
+++ scripts/webglcontext.js
@@ -127,10 +127,89 @@ programEffects = new VokeProgram(new Map([
     varying vec3 vPos;
     uniform mat4 uMVP;
 
+    float rand(vec2 co) {
+        return fract(tan(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
+    }
+    
+    vec3 hash3(vec3 n) {
+        return fract(sin(n) * 1399763.5453123);
+    }
+    
+    vec3 hpos(vec3 n) {
+        return hash3(vec3(dot(n, vec3(157.0, 113.0, 271.0)), dot(n, vec3(311.0, 337.0, 179.0)), dot(n, vec3(271.0, 557.0, 431.0))));
+    }
+    
+    vec4 mod289(vec4 x) {
+        return x - floor(x * (1.0 / 289.0)) * 289.0;
+    }
+    
+    vec4 permute(vec4 x) {
+        return mod(((x * 34.0) + 1.0) * x, 289.0);
+    }
+    
+    vec4 taylorInvSqrt(vec4 r) {
+        return 1.79284291400159 - 0.85373472095314 * r;
+    }
+    
+    vec2 fade(vec2 t) {
+        return t * t * t * (t * (t * 6.0 - 15.0) + 10.0);
+    }
+    
+    float noise4D(vec2 p) {
+        vec4 Pi = floor(p.xyxy) + vec4(0.0, 0.0, 0.0, 1.0);
+        vec4 Pf = fract(p.xyxy) - vec4(0.0, 0.0, 1.0, 1.0);
+    
+        Pi = mod289(Pi);
+    
+        vec4 ix = Pi.xzxz;
+        vec4 iy = Pi.yyww;
+        vec4 fx = Pf.xzxz;
+        vec4 fy = Pf.yyww;
+    
+        vec4 i = permute(permute(ix) + iy);
+        vec4 gx = fract(i * (1.0 / 41.0)) * 2.0 - 1.0;
+        vec4 gy = abs(gx) - 0.5;
+        vec4 tx = floor(gx + 0.5);
+        gx = gx - tx;
+    
+        vec2 g00 = vec2(gx.x, gy.x);
+        vec2 g10 = vec2(gx.y, gy.y);
+        vec2 g01 = vec2(gx.z, gy.z);
+        vec2 g11 = vec2(gx.w, gy.w);
+        vec4 norm = taylorInvSqrt(vec4(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11)));
+        g00 *= norm.x;
+        g01 *= norm.y;
+        g10 *= norm.z;
+        g11 *= norm.z;
+    
+        float n00 = dot(g00, vec2(fx.x, fy.y));
+        float n10 = dot(g10, vec2(fx.y, fy.y));
+        float n01 = dot(g01, vec2(fx.z, fy.z));
+        float n11 = dot(g11, vec2(fx.w, fy.w));
+    
+        vec2 fade_xy = fade(Pf.xy);
+        vec2 n_x = mix(vec2(n00, n01), vec2(n10, n11), fade_xy.x);
+        float n_xy = mix(n_x.x, n_x.y, fade_xy.y);
+        return 2.3 * n_xy;
+    }
+    
+    float noise(vec2 st) {
+        vec2 i = floor(st);
+        vec2 f = fract(st);
+    
+        float a = rand(i);
+        float b = rand(i + vec2(1.0, 0.0));
+        float c = rand(i + vec2(0.0, 1.0));
+        float d = rand(i + vec2(1.0, 1.0));
+    
+        vec2 u = f * f * (3.0 - 2.0 * f);
+        return mix(a, b, u.x) + (c - a) * u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
+    }
+
     void main() {
         gl_Position = uMVP  * vec4(aPos, 1.0);
         vPos = aPos;
-        gl_PointSize = 1.0;
+        gl_PointSize = noise(vec2(0.0, gl_Position.z * 6.0)) * 3.0;
     }
     `, gl.VERTEX_SHADER],
     [`
@@ -220,6 +299,8 @@ programEffects = new VokeProgram(new Map([
     varying vec3 vPos;
 
     void main() {
+        float dist = length(gl_FragCoord.xy - uMousePos);
+
         float f = noise(vec2(length((rand(vec2(-2.0, 663.0))) * 0.2 - vPos.xy * uTickingPos.x)));
         gl_FragColor = (vec4(sin(1.0 - f), noise(vec2(uTickingPos.x * length(rand(vec2(-2.0, 663.0))))), 123233230.0, 1.0)) * (1.0 / vPos.z);
     }`, gl.FRAGMENT_SHADER]
@@ -251,7 +332,7 @@ bufferQuad.attach(0, 3, [0, 0]);
 //bufferQuad.send(new Int32Array(indices), gl.STATIC_DRAW);
 bufferQuad.revoke();
 
-var tickingPos = [0.0, 0.0];
+var tickingPos = [Math.random() * Math.PI * 2.2848, 0.0];
 var trsMatrix = glMatrix.mat4;
 var projMatrix = glMatrix.mat4;
 
@@ -260,8 +341,6 @@ canvas.style.height = "100%"
 
 canvas.width = 1280;
 canvas.height = 720;
-
-console.log(canvas.width);
 
 // the main renderer function.
 function onRender() {
